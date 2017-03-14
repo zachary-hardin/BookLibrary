d5b171d81b5f11385a636e63aa059aa842876dd7
Zachary Hardin <zacharyhardin@Zacharys-MacBook-Pro-4.local>
2017-03-14 11:58:01 -0500

    Created the JavaScript client.

---
+++ BookService/BookService/Scripts/app.js
@@ -0,0 +1,30 @@
+﻿var ViewModel = function() {
+    var self = this;
+    self.books = ko.observableArray();
+    self.error = ko.observable();
+
+    var booksUri = '/api/books';
+
+    function ajaxHelper(uri, method, data) {
+        self.error('');
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            self.error(errorThrown);
+        });
+    }
+
+    function getAllBooks() {
+        ajaxHelper(booksUri, 'GET').done(function(data) {
+            self.books(data);
+        });
+    }
+
+    getAllBooks();
+}
+
+ko.applyBindings(new ViewModel());
