d64653cd4bab716d04dda862463d19d704013e1d
Zachary Hardin <zacharyhardin@Zacharys-MacBook-Pro-4.local>
2017-03-13 19:46:29 -0500

    First commit

---
+++ BookService/BookService/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookService.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+    }
+}
